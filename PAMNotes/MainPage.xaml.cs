aa631552281db799310821177b89e38b0095601c
Jonnathan2020 <jonnathan.santos15@gmail.com>
2024-09-16 20:50:25 -0300

    Mensagem de atualização

---
+++ PAMNotes/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace PAMNotes
+{
+    public partial class MainPage : ContentPage
+    {
+        string filePath = Path.Combine(FileSystem.AppDataDirectory, "Notes.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+            if (File.Exists(filePath))
+            {
+                NoteEditor.Text = File.ReadAllText(filePath);
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            string textoNota = NoteEditor.Text;
+            SemanticScreenReader.Announce(textoNota);
+            Labeltxt.Text = $"Arquivo salvo em {filePath}";
+            DisplayAlert("Alerta", "Arquivo salvo com sucesso!", "OK");
+            File.WriteAllText(filePath, textoNota);
+        }
+
+        private void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                DisplayAlert("Alerta", "Arquivo deletado com sucesso!", "OK");
+                NoteEditor.Text = string.Empty;
+                Labeltxt.Text = "";
+            }
+            else
+            {
+                NoteEditor.Text = string.Empty;
+                DisplayAlert("Alerta", "Arquivo não encontrado", "Ok");
+                
+            }
+        }
+    }
+
+}
